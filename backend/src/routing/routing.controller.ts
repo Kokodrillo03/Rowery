603189f16704200edb19f3e883575d8b616f82e0
Kokodrillo03 <krzychu.biska2@gmail.com>
2025-01-04 20:24:06 +0100

    add cors

--- backend/src/routing/routing.controller.ts
+++ backend/src/routing/routing.controller.ts
@@ -1,10 +1,10 @@
 import {Controller, Get, Param, Query} from "@nestjs/common";
 import {RoutingDto} from "./dto/routing,dto";
+import {RoutingService} from "./routing.service";
 
 @Controller('routing')
 export class RoutingController {
-    private routingService: any;
-    constructor() {}
+    constructor(private readonly routingService: RoutingService) {}
 
     @Get()
     getRoute(@Query() {from, to, bikeType}: RoutingDto) {
