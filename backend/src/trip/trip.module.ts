50a7e4b9998b7f4dea2b60b7a71546cd4fd706e3
kokodrillo03 <krzychu.biska2@gmail.com>
2025-01-06 16:17:11 +0100

    cors

--- backend/src/trip/trip.module.ts
+++ backend/src/trip/trip.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { TripService } from './trip.service'
 import { TripController } from './trip.controller'
 import {TripEntity, TripSchema} from './entities/trip.entity'
+import { HttpModule } from '@nestjs/axios'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: TripEntity.name, schema: TripSchema }]),
+    HttpModule
   ],
   providers: [TripService],
   controllers: [TripController],
